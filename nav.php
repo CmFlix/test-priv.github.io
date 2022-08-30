7094b02662717569306b6bfb7b518a960e83132e
CmFlix <112125008+CmFlix@users.noreply.github.com>
2022-08-30 09:50:19 +0100

    proto nav

---
+++ nav.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal - www.codingnepalweb.com -->
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Proto</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="CSS.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+</head>
+
+<body>
+  <nav>
+    <input type="checkbox" id="check">
+    <label for="check" class="checkbtn">
+      <i class="fas fa-bars"></i>
+    </label>
+    <label class="logo"></label>
+    <ul>
+      <li><a class="active" href="index.php">Accueil</a></li>
+      <li><a href="cantine.php">S'inscrire Cantine</a></li>
+      <li><a href="form.php">Rejoindre le Personnel</a></li>
+      <li><a href="http://emilezolarp.hopto.org/" target="_BLANK_">Accéder à Pronote</a></li>
+    </ul>
+  </nav>
+  <br>
+</body>
+
+</html>
